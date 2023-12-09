928dc5ddec9eada26837ea0b269503c55be1f094
Zunw <43964753+ZunwDev@users.noreply.github.com>
2023-12-09 15:48:37 +0100

    Version 0.13.3.2 Beta

--- src/routes/detail/[id]/[name]/+page.ts
+++ src/routes/detail/[id]/[name]/+page.ts
@@ -1,7 +1,7 @@
 export const prerender = "auto";
 
-export function load({ params }) {
+/* export function load({ params }) {
   if (params.id || params.name) {
     console.log(params.id, params.name);
   }
-}
+} */
